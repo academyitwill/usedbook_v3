e9848e74c976368ba98eee6cebb110fca04034e1
thwjd <thwjd1356@naver.com>
2022-02-22 15:56:23 +0900

    init

---
+++ src/main/java/com/lotu_us/usedbook/UsedbookApplication.java
@@ -0,0 +1,13 @@
+package com.lotu_us.usedbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsedbookApplication.class, args);
+	}
+
+}
