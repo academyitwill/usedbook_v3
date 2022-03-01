3c4092ff47609caf8079fd81a1ccefac57e02b13
thwjd <thwjd1356@naver.com>
2022-03-01 23:34:20 +0900

    httpstatus 추가

--- src/main/java/com/lotu_us/usedbook/util/ErrorResponse.java
+++ src/main/java/com/lotu_us/usedbook/util/ErrorResponse.java
@@ -3,9 +3,11 @@ package com.lotu_us.usedbook.util;
 import com.lotu_us.usedbook.util.exception.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@NoArgsConstructor
 public class ErrorResponse {
     private String cause;
     private String code;
@@ -24,6 +26,7 @@ public class ErrorResponse {
         this.cause = errorCode.getCause();
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
+        this.httpStatus = errorCode.getHttpStatus();
     }
 
 }
