849833bd1fef189249eaf7b5221a6e598e2fc7a4
thwjd <thwjd1356@naver.com>
2022-03-01 23:57:45 +0900

    회원가입 view 구현

--- src/main/java/com/lotu_us/usedbook/config/PasswordRegexpConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lotu_us.usedbook.config;
-
-public class PasswordRegexpConfig {
-    private static final String allowNumber = "(?=.*[0-9])";
-    private static final String allowEnglish = "(?=.*[a-z])";
-    private static final String allowSpecial = "(?=.*[!\"#$%&'()*+,-./:;<=>?@^_`{|}\\[\\]~\\\\])(?=\\S+$)";
-    private static final String NotAllowSpace = "(?=\\S+$)";
-    public static final String reg = "^"+allowNumber+allowEnglish+allowSpecial+NotAllowSpace+".{5,20}$";
-}
