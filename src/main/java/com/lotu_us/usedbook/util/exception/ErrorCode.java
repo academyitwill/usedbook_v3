e59f08335a0b40fdd7a565b20b9dc9f80a7ee091
thwjd <thwjd1356@naver.com>
2022-03-01 23:32:48 +0900

    글로벌에러, 로컬에러 삭제 후 ErrorCode 추가하는 것으로 변경
    더 복잡해지는 것 같아 변경

--- src/main/java/com/lotu_us/usedbook/util/exception/ErrorCode.java
+++ src/main/java/com/lotu_us/usedbook/util/exception/ErrorCode.java
@@ -6,7 +6,16 @@ public enum ErrorCode {
     //global
     DATABASE_ERROR("database", "database", "데이터베이스 에러가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     PASSWORD_NOT_EQUAL("password", "password.notequal", "패스워드가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
-    ID_NOT_FOUND("id", "id.notfound", "해당 id를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST)
+    ID_NOT_FOUND("id", "id.notfound", "해당 id를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+
+
+    //member
+    EMAIL_DUPLICATED("email", "email.duplicated", "이메일이 중복됩니다.", HttpStatus.BAD_REQUEST),
+    NICKNAME_DUPLICATED("nickname", "nickname.duplicated", "닉네임이 중복됩니다.", HttpStatus.BAD_REQUEST),
+    NICKNAME_EQUAL_PREVIOUS("nickname", "nickname.equal.previous", "닉네임이 이전과 같습니다.", HttpStatus.BAD_REQUEST),
+    PASSWORD_EQUAL_PREVIOUS("newPassword", "password.equal.previous", "기존 비밀번호와 새 비밀번호가 일치합니다.", HttpStatus.BAD_REQUEST),
+    EMAIL_NOT_FOUND("email", "email.notfound", "해당 이메일을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
+    NICKNAME_NOT_EQUAL("nickname", "nickname.notequal", "닉네임이 일치하지 않습니다.", HttpStatus.BAD_REQUEST)
     ;
 
     private String cause;
