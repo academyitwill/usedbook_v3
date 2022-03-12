1bc21ab98aecbb414d2882525e534e5230f7e98f
thwjd <thwjd1356@naver.com>
2022-03-12 23:24:25 +0900

    테스트코드 수정

--- src/test/java/com/lotu_us/usedbook/controller/MemberApiControllerTest.java
+++ src/test/java/com/lotu_us/usedbook/controller/MemberApiControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.transaction.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class MemberApiControllerTest {
     @Autowired private MockMvc mock;
     @Autowired private ObjectMapper objectMapper;
